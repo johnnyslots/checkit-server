afe44daeef5d7c72ef50bb3f04a7f54f04a57eb1
Yoni Slotwinere <yonislots@gmail.com>
2018-04-27 13:06:07 -0400

    sockets setup for backend

--- server/socket/index.js
+++ server/socket/index.js
@@ -3,9 +3,10 @@ module.exports = (io) => {
     console.log(`A socket connection to the server has been made: ${socket.id}`)
 
     socket.on('newRec', (data) => {
-      console.log('SOCKETS!!', data)
-      socket.emit(data)
+      // console.log('SOCKETS!!', data)
+      socket.broadcast.emit('newRec', data)
     })
+
     socket.on('disconnect', () => {
       console.log(`Connection ${socket.id} has left the building`)
     })
