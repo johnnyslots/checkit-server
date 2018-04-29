6a5f8ded424e34e293074512198b05b81a5a9aad
Yoni Slotwinere <yonislots@gmail.com>
2018-04-28 20:18:44 -0400

    preparing for requestRec socket

--- server/socket/index.js
+++ server/socket/index.js
@@ -1,9 +1,16 @@
+const {User} = require('../db/models')
+
 module.exports = (io) => {
   io.on('connection', (socket) => {
     console.log(`A socket connection to the server has been made: ${socket.id}`)
 
     socket.on('newRec', (data) => {
       socket.broadcast.emit('newRec', data)
+    })
+
+    socket.on('requestRec', (data) => {
+      console.log('backend socket data!', data)
+      socket.broadcast.emit('requestRec', data)
     })
 
     socket.on('disconnect', () => {
