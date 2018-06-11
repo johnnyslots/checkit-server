5f55b768d4e99ca3399e8588288dd017d65538af
Yoni Slotwinere <yonislots@gmail.com>
2018-06-11 15:30:29 -0400

    /pending route for pending recs

--- server/api/recommendations.js
+++ server/api/recommendations.js
@@ -14,19 +14,46 @@ router.get('/', (req, res, next) => {
   .catch(next)
 })
 
-router.get('/pending/:id', (req, res, next) => {
-  const id = req.params.id
+router.get('/:category/users/:userId', (req, res, next) => {
+  const category = req.params.category
+  const userId = req.params.userId
   Recommendation.findAll({
     where: {
-      isPending: true,
-      toId: id
+      isPending: false,
+      toId: userId
     },
     include: [
       {all: true}
     ]
   })
   .then(recs => {
-    res.json(recs)
+
+    const filteredByCategory = recs.filter(rec => {
+      return rec.item.category === category;
+    })
+    res.json(filteredByCategory);
+  })
+  .catch(next)
+})
+
+router.get('/:category/users/:userId/pending', (req, res, next) => {
+  const category = req.params.category
+  const userId = req.params.userId
+  Recommendation.findAll({
+    where: {
+      isPending: true,
+      toId: userId
+    },
+    include: [
+      {all: true}
+    ]
+  })
+  .then(pendingRecs => {
+
+    const filteredByCategory = pendingRecs.filter(rec => {
+      return rec.item.category === category;
+    })
+    res.json(filteredByCategory);
   })
   .catch(next)
 })
@@ -54,102 +81,80 @@ router.post('/ownRec', (req, res, next) => {
   .catch(next)
 })
 
-router.post('/', (req, res, next) => {
-  const {category, title, notes} = req.body.postData
-  const fromId = req.body.postData.sender ? req.body.postData.sender.id : null
-  let toId = req.body.user ? req.body.user.id : null
-  const email = req.body.userEmail
-  const isPending = req.body.postData.sender ? true : false
-  if(!toId) {
-    User.findOne({
-      where: {
-        email
-      }
-    })
-    .then(user => {
-      toId = user.id
-    })
-  }
-  ListItem.findOrCreate({
-    where: {
-      category,
-      title
-    }
-  })
-  .spread((item, create) => {
-    return Recommendation.create({
-      notes,
-      itemId: item.id,
-      fromId,
-      toId,
-      isPending
-    })
-  })
-  .then(() => {
-    Recommendation.findAll({
-      where: {
-        isPending: false,
-        toId
-      },
-      include: [
-        {all: true}
-      ]
-    })
-    .then(recs => {
-      res.json(recs)
-    })
-  })
-  .catch(next)
-})
+// router.post('/', (req, res, next) => {
+//   const {category, title, notes} = req.body.postData
+//   const fromId = req.body.postData.sender ? req.body.postData.sender.id : null
+//   let toId = req.body.user ? req.body.user.id : null
+//   const email = req.body.userEmail
+//   const isPending = req.body.postData.sender ? true : false
+//   if(!toId) {
+//     User.findOne({
+//       where: {
+//         email
+//       }
+//     })
+//     .then(user => {
+//       toId = user.id
+//     })
+//   }
+//   ListItem.findOrCreate({
+//     where: {
+//       category,
+//       title
+//     }
+//   })
+//   .spread((item, create) => {
+//     return Recommendation.create({
+//       notes,
+//       itemId: item.id,
+//       fromId,
+//       toId,
+//       isPending
+//     })
+//   })
+//   .then(() => {
+//     Recommendation.findAll({
+//       where: {
+//         isPending: false,
+//         toId
+//       },
+//       include: [
+//         {all: true}
+//       ]
+//     })
+//     .then(recs => {
+//       res.json(recs)
+//     })
+//   })
+//   .catch(next)
+// })
 
-router.put('/pending/:id', (req, res, next) => {
-  const id = req.params.id
-  Recommendation.findById(id)
-  .then(rec => {
-    rec.update({
-      isPending: false
-    })
-  })
-  .then(updatedRec => {
-    res.status(200).send(updatedRec)
-  })
-  .catch(next)
-})
+// router.put('/pending/:id', (req, res, next) => {
+//   const id = req.params.id
+//   Recommendation.findById(id)
+//   .then(rec => {
+//     rec.update({
+//       isPending: false
+//     })
+//   })
+//   .then(updatedRec => {
+//     res.status(200).send(updatedRec)
+//   })
+//   .catch(next)
+// })
 
-router.delete('/:id', (req, res, next) => {
-  const id = req.params.id
-  Recommendation.findById(id)
-  .then(rec => {
-    ListItem.findById(rec.itemId)
-    .then(listItem => {
-      listItem.destroy()
-      rec.destroy()
-    })
-    .then((deletedRec) => {
-      res.status(201).send(deletedRec)
-    })
-    .catch(next)
-  })
-})
-
-router.get('/:category/users/:userId', (req, res, next) => {
-  const category = req.params.category
-  const userId = req.params.userId
-  Recommendation.findAll({
-    where: {
-      isPending: false,
-      toId: userId
-    },
-    include: [
-      {all: true}
-    ]
-  })
-  .then(recs => {
-
-    const filteredByCategory = recs.filter(rec => {
-      return rec.item.category === category;
-    })
-    res.json(filteredByCategory);
-  })
-  .catch(next)
-})
+// router.delete('/:id', (req, res, next) => {
+//   const id = req.params.id
+//   Recommendation.findById(id)
+//   .then(rec => {
+//     ListItem.findById(rec.itemId)
+//     .then(listItem => {
+//       listItem.destroy()
+//       rec.destroy()
+//     })
+//     .then((deletedRec) => {
+//       res.status(201).send(deletedRec)
+//     })
+//     .catch(next)
+//   })
+// })
